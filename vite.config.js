0f6b67beed74cecb89f652ddad86d28b17f85e40
unknown <lhn1208@naver.com>
2025-04-06 18:47:34 +0900

    수정

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/style/global.scss";`
+        additionalData: `@use "@/style/global.scss";`
       }
     }
   },
